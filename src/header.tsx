bacbf9f123ef983939d038a2c66decda526c9dbc
Nana Mochihara <nana.mochihara@gmail.com>
2018-07-28 22:10:45 +0200

    Exchange logo and brand name

--- src/header.tsx
+++ src/header.tsx
@@ -1,13 +1,12 @@
 import * as React from 'react';
 
-import logo from './logo.png';
+import logo from './logoAndBrand.png';
 
 class Header extends React.Component {
   public render() {
     return (
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Graphy</h1>
+        <img src={logo} alt="logo" />
       </header>
     );
   }
