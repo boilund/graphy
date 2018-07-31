ed94181a2b6a270d2f352ff1158b6b3fc45098ef
Nana Mochihara <nana.mochihara@gmail.com>
2018-07-31 13:06:09 +0200

    Import icons of fontawesome

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faChartArea, faChartBar, faChartLine, faChartPie, faSignal } from '@fortawesome/free-solid-svg-icons';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './index.css'
 import App from './js/App';
 import registerServiceWorker from './registerServiceWorker';
+
+library.add(faChartArea, faChartBar, faChartLine, faChartPie, faSignal);
 
 ReactDOM.render(
   <App />,
