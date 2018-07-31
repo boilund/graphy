f0243f46a863abd1f32963ce2ba800b3298d1e44
Nana Mochihara <nana.mochihara@gmail.com>
2018-07-31 05:45:12 +0200

    Create navbar

--- src/js/header.tsx
+++ src/js/header.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { MenuItem, Nav, Navbar, NavDropdown, NavItem } from 'react-bootstrap';
+import './header.css';
 
 import logo from './logoAndBrand.png';
 
@@ -6,7 +8,25 @@ class Header extends React.Component {
   public render() {
     return (
       <header className="App-header">
-        <img src={logo} alt="logo" />
+        <Navbar className="navbar">
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href='#home'><img src={logo} alt="logo" /></a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav className="nav">
+            <NavItem eventKey={1} href="#">
+              My graphs
+            </NavItem>
+            <NavItem eventKey={2} href="#">
+              Share
+            </NavItem>
+            <NavDropdown eventKey={3} title="Login" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}>Login with your account</MenuItem>
+              <MenuItem eventKey={3.2}>Create new account</MenuItem>
+            </NavDropdown>
+          </Nav>
+        </Navbar>
       </header>
     );
   }
