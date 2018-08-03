9c4e8def092fdb19786a2370c97da839e73881c5
Nana Mochihara <nana.mochihara@gmail.com>
2018-08-03 13:10:27 +0200

    Basic template creating chart flow

--- src/js/creat-new-graph.tsx
+++ src/js/creat-new-graph.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Col, ControlLabel, FormControl, Grid, Row, Table } from 'react-bootstrap';
 import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const data = [
@@ -10,7 +11,29 @@ const data = [
   {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
   {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
 ];
+
+interface IState {
+  value: number;
+}
+
+class CreateNewGraph extends React.Component<{}, IState> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      value: 100,
+    }
+  }
+
+  public handleChange = (e: any): void => {
+    this.setState({
+      value: e.target.value,
+    });
+  }
+
   public render() {
+    const { value } = this.state;
+
     return (
       <main className="main">
         <Grid>
@@ -31,6 +54,170 @@ const data = [
               </LineChart>
             </Col>
           </Row>
+          <Row>
+            <Col sm={12}>
+              <h3>Data set</h3>
+              <Table striped={true} bordered={true} condensed={true} hover={true}>
+                <thead>
+                  <tr>
+                    <th>
+                      <FormControl
+                          type="text"
+                          // value={value}
+                          placeholder="Enter title"
+                          // onChange={this.handleChange}
+                      />
+                    </th>
+                    <th>
+                      <FormControl
+                          type="text"
+                          // value={value}
+                          placeholder="Enter title"
+                          // onChange={this.handleChange}
+                      />
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      <FormControl
+                        type="text"
+                        value={value}
+                        placeholder="value"
+                        onChange={this.handleChange}
+                        />
+                    </td>
+                    <td>
+                      <FormControl
+                          type="text"
+                          // value={value}
+                          placeholder="value"
+                          // onChange={this.handleChange}
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                  <td>
+                      <FormControl
+                        type="text"
+                        // value={value}
+                        placeholder="value"
+                        // onChange={this.handleChange}
+                        />
+                    </td>
+                    <td>
+                      <FormControl
+                          type="text"
+                          // value={value}
+                          placeholder="value"
+                          // onChange={this.handleChange}
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <FormControl
+                        type="text"
+                        // value={value}
+                        placeholder="value"
+                        // onChange={this.handleChange}
+                        />
+                    </td>
+                    <td>
+                      <FormControl
+                          type="text"
+                          // value={value}
+                          placeholder="value"
+                          // onChange={this.handleChange}
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <FormControl
+                        type="text"
+                        // value={value}
+                        placeholder="value"
+                        // onChange={this.handleChange}
+                        />
+                    </td>
+                    <td>
+                      <FormControl
+                          type="text"
+                          // value={value}
+                          placeholder="value"
+                          // onChange={this.handleChange}
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <FormControl
+                        type="text"
+                        // value={value}
+                        placeholder="value"
+                        // onChange={this.handleChange}
+                        />
+                    </td>
+                    <td>
+                      <FormControl
+                          type="text"
+                          // value={value}
+                          placeholder="value"
+                          // onChange={this.handleChange}
+                      />
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={12}>
+              <h3>X and Y set</h3>
+            </Col>
+            <Col sm={6}>
+              <ControlLabel>Select X</ControlLabel>
+                <FormControl componentClass="select" placeholder="select">
+                  <option value="select">select</option>
+                  <option value="other">...</option>
+                </FormControl>
+            </Col>
+            <Col sm={6}>
+              <ControlLabel>Select Y</ControlLabel>
+                <FormControl componentClass="select" placeholder="select">
+                  <option value="select">select</option>
+                  <option value="other">...</option>
+                </FormControl>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={12}>
+              <h3>Choose chart type</h3>
+            </Col>
+            <Col sm={2}>
+              <Button bsStyle="primary" bsSize="large">Line chart</Button>
+            </Col>
+            <Col sm={2}>
+              <Button bsStyle="primary" bsSize="large">Bar chart</Button>
+            </Col>
+            <Col sm={2}>
+              <Button bsStyle="primary" bsSize="large">Area chart</Button>
+            </Col>
+            <Col sm={2}>
+              <Button bsStyle="primary" bsSize="large">Pie chart</Button>
+            </Col>
+            <Col sm={2}>
+              <Button bsStyle="primary" bsSize="large">Radar chart</Button>
+            </Col>
+            <Col sm={2}>
+              <Button bsStyle="primary" bsSize="large">Scatter chart</Button>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={12}>
+              <h3>Completed!</h3>
             </Col>
           </Row>
         </Grid>
