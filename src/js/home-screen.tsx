ffd9de69ec9faa6c8c7b4e43d1b586ee20f94d27
Nana Mochihara <nana.mochihara@gmail.com>
2018-08-16 21:42:30 +0200

    Formatted by prettier and move link making graph to the icon

--- src/js/home-screen.tsx
+++ src/js/home-screen.tsx
@@ -1,47 +1,58 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import * as React from 'react';
-import { Button, Col, Grid, Row } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import './home-screen.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import * as React from "react";
+import { Col, Grid, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./home-screen.css";
 
 class HomeScreen extends React.Component {
   public render() {
     return (
       <main className="main">
-        <Grid>
+        <Grid fluid={true}>
           <Row>
-            <Col sm={12}>
-              <Button>
-                <Link to='./creating' >Create graph</Link>
-              </Button>
+            <Col sm={3} className="yellow">
+              <div>
+                <FontAwesomeIcon icon="signal" className="fa-6x icons" />
+              </div>
+            </Col>
+            <Col sm={3} className="green">
+              <div>
+                <FontAwesomeIcon icon="chart-pie" className="fa-6x icons" />
+              </div>
+            </Col>
+            <Col sm={3} className="purple">
+              <div>
+                <FontAwesomeIcon icon="chart-bar" className="fa-6x icons" />
+              </div>
+            </Col>
+            <Col sm={3} className="red">
+              <div>
+                <FontAwesomeIcon icon="chart-area" className="fa-6x icons" />
+              </div>
             </Col>
           </Row>
           <Row>
-            <Col sm={3} className="yellow">
-              <div><FontAwesomeIcon icon="signal" className="fa-6x icons" /></div>
-            </Col>
-            <Col sm={3} className="green">
-              <div><FontAwesomeIcon icon="chart-pie" className="fa-6x icons" /></div>
-            </Col>
             <Col sm={3} className="purple">
-              <div><FontAwesomeIcon icon="chart-bar" className="fa-6x icons" /></div>
+              <Link to="./creating">
+                <div>
+                  <FontAwesomeIcon icon="chart-line" className="fa-6x icons" />
+                </div>
+              </Link>
             </Col>
             <Col sm={3} className="red">
-              <div><FontAwesomeIcon icon="chart-area" className="fa-6x icons" /></div>
-            </Col>
-          </Row>
-          <Row>
-            <Col sm={3} className="purple">
-              <div><FontAwesomeIcon icon="chart-line" className="fa-6x icons" /></div>
-            </Col>
-            <Col sm={3} className="red">
-              <div><FontAwesomeIcon icon="signal" className="fa-6x icons" /></div>
+              <div>
+                <FontAwesomeIcon icon="signal" className="fa-6x icons" />
+              </div>
             </Col>
             <Col sm={3} className="yellow">
-              <div><FontAwesomeIcon icon="chart-area" className="fa-6x icons" /></div>
+              <div>
+                <FontAwesomeIcon icon="chart-area" className="fa-6x icons" />
+              </div>
             </Col>
             <Col sm={3} className="green">
-              <div><FontAwesomeIcon icon="chart-pie" className="fa-6x icons" /></div>
+              <div>
+                <FontAwesomeIcon icon="chart-pie" className="fa-6x icons" />
+              </div>
             </Col>
           </Row>
         </Grid>
