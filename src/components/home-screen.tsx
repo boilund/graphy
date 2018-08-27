4bacb1cf1aec97903327a3dea34de27bc265cf7d
Nana Mochihara <nana.mochihara@gmail.com>
2018-08-27 14:24:26 +0200

    Add style and able to click anywhere on the square

--- src/components/home-screen.tsx
+++ src/components/home-screen.tsx
@@ -10,50 +10,44 @@ class HomeScreen extends React.Component {
       <main className="App-main">
         <Grid fluid={true}>
           <Row>
-            <Col sm={3} className="yellow">
+            <Col sm={3} className="yellow square">
               <div>
                 <FontAwesomeIcon icon="signal" className="fa-6x icons" />
               </div>
             </Col>
-            <Col sm={3} className="green">
+            <Col sm={3} className="green square">
               <div>
                 <FontAwesomeIcon icon="chart-pie" className="fa-6x icons" />
               </div>
             </Col>
-            <Col sm={3} className="purple">
-              <Link to="./creating/bar-graph">
-                <div>
-                  <FontAwesomeIcon icon="chart-bar" className="fa-6x icons" />
-                </div>
-              </Link>
-            </Col>
-            <Col sm={3} className="red">
-              <Link to="./creating/area-graph">
-                <div>
-                  <FontAwesomeIcon icon="chart-area" className="fa-6x icons" />
-                </div>
-              </Link>
-            </Col>
+            <Link to="./creating/bar-graph">
+              <Col sm={3} className="purple square">
+                <FontAwesomeIcon icon="chart-bar" className="fa-6x icons" />
+              </Col>
+            </Link>
+            <Link to="./creating/area-graph">
+              <Col sm={3} className="red square">
+                <FontAwesomeIcon icon="chart-area" className="fa-6x icons" />
+              </Col>
+            </Link>
           </Row>
           <Row>
-            <Col sm={3} className="purple">
-              <Link to="./creating/line-graph">
-                <div>
-                  <FontAwesomeIcon icon="chart-line" className="fa-6x icons" />
-                </div>
-              </Link>
-            </Col>
-            <Col sm={3} className="red">
+            <Link to="./creating/line-graph">
+              <Col sm={3} className="purple square">
+                <FontAwesomeIcon icon="chart-line" className="fa-6x icons" />
+              </Col>
+            </Link>
+            <Col sm={3} className="red square">
               <div>
                 <FontAwesomeIcon icon="signal" className="fa-6x icons" />
               </div>
             </Col>
-            <Col sm={3} className="yellow">
+            <Col sm={3} className="yellow square">
               <div>
                 <FontAwesomeIcon icon="chart-area" className="fa-6x icons" />
               </div>
             </Col>
-            <Col sm={3} className="green">
+            <Col sm={3} className="green square">
               <div>
                 <FontAwesomeIcon icon="chart-pie" className="fa-6x icons" />
               </div>
