7b1a605ed2965a7f38d51c88fcc063cb245bf7d5
Nana Mochihara <nana.mochihara@gmail.com>
2018-08-29 21:47:49 +0200

    misc

--- src/components/graphs/area-graph.tsx
+++ src/components/graphs/area-graph.tsx
@@ -27,26 +27,27 @@ const AreaGraph: React.SFC<IProps> = (props: IProps) => {
           <h3>{title ? title : "Area Graph"}</h3>
         </Col>
       </Row>
-      <AreaChart
-        width={600}
-        height={400}
-        data={data.slice()}
-        margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-        className="center"
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="columnX" />
-        <YAxis />
-        <Tooltip />
-        <Area
-          type="monotone"
-          dataKey="columnY"
-          name={yAxis}
-          stroke="#8884d8"
-          fill="#8884d8"
-        />
-      </AreaChart>
-      <Row />
+      <Row>
+        <AreaChart
+          width={600}
+          height={400}
+          data={data.slice()}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+          className="center"
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="columnX" />
+          <YAxis />
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="columnY"
+            name={yAxis}
+            stroke="#8884d8"
+            fill="#8884d8"
+          />
+        </AreaChart>
+      </Row>
     </React.Fragment>
   );
 };
