9ee2a9c341716d183d9c7470b2ab90207eb1f033
Nana Mochihara <nana.mochihara@gmail.com>
2018-10-11 12:55:24 +0200

    Adjust home screen page without radar-graph

--- src/components/home-screen.tsx
+++ src/components/home-screen.tsx
@@ -4,7 +4,6 @@ import { Col, Grid, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./home-screen.css";
 
-import radar from "../imgs/radar-icon.svg";
 import scatter from "../imgs/scatter-icon.svg";
 
 class HomeScreen extends React.Component {
@@ -13,9 +12,9 @@ class HomeScreen extends React.Component {
       <main className="App-main">
         <Grid>
           <Row>
-            <Col xs={6} sm={4} md={3}>
+            <Col xs={6} sm={4}>
               <div className="yellow comment-square">
-                <h2 className="message">Select graph type</h2>
+                <h2 className="message">Select graph</h2>
                 <FontAwesomeIcon
                   icon="comment"
                   className="fa-6x comment-icon"
@@ -23,52 +22,40 @@ class HomeScreen extends React.Component {
               </div>
             </Col>
             <Link to="./creating/line-graph">
-              <Col xs={6} sm={4} md={3}>
+              <Col xs={6} sm={4}>
                 <div className="square green">
                   <FontAwesomeIcon icon="chart-line" className="fa-6x icons" />
                 </div>
               </Col>
             </Link>
             <Link to="./creating/bar-graph">
-              <Col xs={6} sm={4} md={3}>
+              <Col xs={6} sm={4}>
                 <div className="square purple">
                   <FontAwesomeIcon icon="chart-bar" className="fa-6x icons" />
                 </div>
               </Col>
             </Link>
             <Link to="./creating/area-graph">
-              <Col xs={6} sm={4} md={3}>
-                <div className="square red">
+              <Col xs={6} sm={4}>
+                <div className="square purple">
                   <FontAwesomeIcon icon="chart-area" className="fa-6x icons" />
                 </div>
               </Col>
             </Link>
             <Link to="./creating/pie-graph">
-              <Col xs={6} sm={4} md={3}>
-                <div className="square green">
+              <Col xs={6} sm={4}>
+                <div className="square yellow">
                   <FontAwesomeIcon icon="chart-pie" className="fa-6x icons" />
                 </div>
               </Col>
             </Link>
             <Link to="./creating/scatter-graph">
-              <Col xs={6} sm={4} md={3}>
+              <Col xs={6} sm={4}>
                 <div className="square red">
                   <img src={scatter} alt="scatter graph" className="icons" />
                 </div>
               </Col>
             </Link>
-            <Link to="./creating/radar-graph">
-              <Col xs={6} sm={4} md={3}>
-                <div className="square yellow">
-                  <img src={radar} alt="radar graph" className="radar icons" />
-                </div>
-              </Col>
-            </Link>
-            <Col xs={6} sm={4} md={3}>
-              <div className="square purple">
-                <FontAwesomeIcon icon="chart-pie" className="fa-6x icons" />
-              </div>
-            </Col>
           </Row>
         </Grid>
       </main>
