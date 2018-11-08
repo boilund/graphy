4c4eddf06030f43a40639fb37b6e99d399451e7e
Nana Mochihara <nana.mochihara@gmail.com>
2018-11-08 14:56:34 +0100

    Update login page style

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,72 +1,30 @@
 import * as React from "react";
-import {
-  Button,
-  Col,
-  ControlLabel,
-  FormControl,
-  FormGroup,
-  Grid,
-  HelpBlock,
-  Row
-} from "react-bootstrap";
+import { Button, Col, Grid, Row } from "react-bootstrap";
+import { firebaseAuth, firebaseGoogleProvider } from "../firebase";
 import "./login.css";
 
-interface IState {
-  password: string;
-  username: string;
-}
+import google from "../imgs/google-logo.png";
 
-class Login extends React.Component<{}, IState> {
+class Login extends React.Component<{}, {}> {
   constructor(props: any) {
     super(props);
-    this.state = {
-      password: "",
-      username: ""
-    };
-    this.handleUsernameChange = this.handleUsernameChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.login = this.login.bind(this);
   }
 
   public render() {
-    const { password, username } = this.state;
     return (
       <main className="App-main login-main">
         <Grid>
           <Row className="login-row">
             <Col xs={8} xsOffset={2} sm={4} smOffset={4} className="login-box">
-              <h1>Login to Graphy</h1>
-              <form>
-                <FormGroup
-                  controlId="formControlsUsername"
-                  className="text-left"
-                >
-                  <ControlLabel>Username</ControlLabel>
-                  <FormControl
-                    id="formControlsUsername"
-                    type="text"
-                    value={username}
-                    onChange={this.handleUsernameChange}
-                  />
-                  <FormControl.Feedback />
-                </FormGroup>
-                <FormGroup
-                  controlId="formControlsPassword"
-                  className="text-left"
-                >
-                  <ControlLabel>Password</ControlLabel>
-                  <FormControl
-                    id="formControlsPassword"
-                    type="password"
-                    value={password}
-                    onChange={this.handlePasswordChange}
-                  />
-                  <FormControl.Feedback />
-                  <HelpBlock className="text-right">Forgot password?</HelpBlock>
-                </FormGroup>
-              </form>
-              <Button className="btn-block login-btn">Login</Button>
+              <h1 className="login-box-header">Login to Graphy</h1>
+
+              <Button className="btn-block login-btn" onClick={this.login}>
+                <img className="google-logo" src={google} alt="Google logo" />
+                Login with Google account
+              </Button>
               <Button className="btn-block signin-btn">
-                Create an account
+                New to Graphy? Create an account
               </Button>
             </Col>
           </Row>
@@ -75,15 +33,7 @@ class Login extends React.Component<{}, IState> {
     );
   }
 
-  private handleUsernameChange = (e: any): void => {
-    this.setState({
-      username: e.target.value
-    });
-  };
-
-  private handlePasswordChange = (e: any): void => {
-    this.setState({
-      password: e.target.value
+  private login = (e: any): void => {
     });
   };
 }
