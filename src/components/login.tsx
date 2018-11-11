61636de9a60413287960308b36604aba66c06b51
Nana Mochihara <nana.mochihara@gmail.com>
2018-11-11 22:05:27 +0100

    Show logout button if user logged in

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -21,6 +21,7 @@ class Login extends React.Component<IProps, {}> {
   constructor(props: any) {
     super(props);
     this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
   }
 
   public componentDidMount() {
@@ -68,20 +69,35 @@ class Login extends React.Component<IProps, {}> {
   }
 
   public render() {
+    const { loginState } = this.props;
+
     return (
       <main className="App-main login-main">
         <Grid>
           <Row className="login-row">
             <Col xs={8} xsOffset={2} sm={4} smOffset={4} className="login-box">
-              <h1 className="login-box-header">Login to Graphy</h1>
-
+              <h1 className="login-box-header">
+                {loginState ? "Logout from Graphy" : "Login to Graphy"}
+              </h1>
+              {loginState ? (
+                <Button className="logout-btn" onClick={this.logout}>
+                  Logout
+                </Button>
+              ) : (
+                <React.Fragment>
               <Button className="btn-block login-btn" onClick={this.login}>
-                <img className="google-logo" src={google} alt="Google logo" />
+                    <img
+                      className="google-logo"
+                      src={google}
+                      alt="Google logo"
+                    />
                 Login with Google account
               </Button>
               <Button className="btn-block signin-btn">
                 New to Graphy? Create an account
               </Button>
+                </React.Fragment>
+              )}
             </Col>
           </Row>
         </Grid>
